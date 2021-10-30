0dfe477ab561f175a6c59baf620ec5362bc9e786
Svetlana <svetlananepoklonova@gmail.com>
2021-10-30 18:35:27 +0300

    Templation done

--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -3,18 +3,26 @@
 #include<ctime>		//Такая же библиотека, написанная на языке C++
 using namespace std;
 
+#define delimiter "\n------------------------------------------------\n"
+
 const unsigned int ROWS = 4;
 const unsigned int COLS = 5;
 
 void FillRand(int arr[], const unsigned int n, int minRand = 0, int maxRand = 100);	//Заполняет массив случайными числами
 void FillRand(double arr[], const unsigned int n, int minRand = 0, int maxRand = 100);	//Заполняет массив случайными числами
-void Print(int arr[], const unsigned int n);
-void Print(double arr[], const unsigned int n);
-void Print(int arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS);
+
+template<typename T>void Print(T arr[], const unsigned int n);
+template<typename T>void Print(T arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS);
+
+template<typename T>T Sum(T arr[], const unsigned int n);
+void Sum(char arr[], const unsigned int n);
 
 void main()
 {
 	setlocale(LC_ALL, "");
+
+	char num = -1;
+
 	const unsigned int n = 5;
 	int arr[n];
 	//srand(5);
@@ -23,9 +31,13 @@ void main()
 	//cout << "Введите максимальное случайное число: "; cin >> maxRand;
 	FillRand(arr, n, 200,300);
 	Print(arr, n);
+	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << delimiter << endl;
 	double brr[n];
 	FillRand(brr, n);
 	Print(brr, n);
+	cout << "Сумма элементов массива: " << Sum(brr, n) << endl;
+	cout << delimiter << endl;
 
 
 	int i_arr_2[ROWS][COLS] = 
@@ -35,6 +47,14 @@ void main()
 		{7,8,9}
 	};
 	Print(i_arr_2, ROWS, COLS);
+	cout << delimiter << endl;
+
+	double d_arr_2[ROWS][COLS] = {
+		{2.2, 3.14, 8, 3},
+		{4.2, 7.3, 9.4 },
+		{.25}
+	};
+	Print(d_arr_2, ROWS, COLS);
 }
 
 void FillRand(int arr[], const unsigned int n, int minRand, int maxRand)
@@ -55,7 +75,7 @@ void FillRand(double arr[], const unsigned int n, int minRand, int maxRand)
 	}
 }
 
-void Print(int arr[], const unsigned int n)
+template<typename T>void Print(T arr[], const unsigned int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -63,15 +83,8 @@ void Print(int arr[], const unsigned int n)
 	}
 	cout << endl;
 }
-void Print(double arr[], const unsigned int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-}
-void Print(int arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS)
+
+template<typename T>void Print(T arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS)
 {
 	for (int i = 0; i < ROWS; i++)
 	{
@@ -81,4 +94,18 @@ void Print(int arr[ROWS][COLS], const unsigned int ROWS, const unsigned int COLS
 		}
 		cout << endl;
 	}
+}
+
+template<typename T>T Sum(T arr[], const unsigned int n)
+{
+	T sum = T(); // T() - значение по умолчанию для шаблонного типа данных
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return  sum;
+}
+
+void Sum(char arr[], const unsigned int n) {
+	cout << "Буквы суммировать не стоит.";
 }
